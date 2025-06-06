ed2564fc58a27f9186f643015bd82af3c4c249be
Ashmit <ashmit27j@gmail.com>
2025-06-06 15:10:43 +0530

    Refactored file structure, Update: learned basic tags iin swift ui, on track to creating first mini project

---
+++ myFirstSwiftUIApp/MySwiftUIApp/MySwiftUIAppApp.swift
@@ -0,0 +1,16 @@
+//
+//  MySwiftUIAppApp.swift
+//  MySwiftUIApp
+//
+//  Created by MacBook on 05/06/25.
+//
+
+import SwiftUI
+@main
+struct MySwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myFirstSwiftUIApp/MySwiftUIAppTests/MySwiftUIAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIAppTests.swift
+//  MySwiftUIAppTests
+//
+//  Created by MacBook on 05/06/25.
+//
+
+import Testing
+@testable import MySwiftUIApp
+
+struct MySwiftUIAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
